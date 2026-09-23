a60804be0ad53a692a33e52062a37e02363b2a3a
minhthinh-GIT <minhthinh20112003@gmail.com>
2026-09-23 21:55:55 +0700

    Buoi02 Bai02 gioi thieu ban than

---
+++ Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,9 @@
+// Họ tên: Đoàn Minh Thịnh - MSSV: 25810040
+fun main() {
+    val hoTen: String = "Đoàn Minh Thịnh"
+    val namSinh: Int = 2003
+    val namHienTai: Int = 2026
+    val tuoi: Int = namHienTai - namSinh
+
+    println("Tôi tên là $hoTen, năm nay tôi $tuoi tuổi.")
+}
