934388785d4533d2d72d58797649a66b36bd6c2c
minhthinh-GIT <minhthinh20112003@gmail.com>
2026-09-23 22:35:17 +0700

    Buoi02 Bai07 dem nguoc va repeat
    Implement main function with while and repeat loops.

---
+++ Bai07_WhileVaRepeat.kt
@@ -0,0 +1,17 @@
+// Họ tên: Đoàn Minh Thịnh - MSSV: 25810040
+fun main() {
+    var i: Int = 10
+    while (i >= 1) {
+        println(i)
+        i--
+    }
+
+    repeat(5) {
+        println("Hello World")
+    }
+}
+
+/*
+  Dùng while khi không bắt buộc biết trước số lần lặp, phụ thuộc vào một điều kiện
+  Dùng repeat khi đã biết trước số lần muốn thực hiện 
+*/
