23006c45c6391f9be55a1141d132a24f46eb9cbf
minhthinh-GIT <minhthinh20112003@gmail.com>
2026-09-23 21:50:13 +0700

    Buoi02 Bai01 tinh tien mua hang

---
+++ Bai01_TinhTienMuaHang.kt
@@ -0,0 +1,10 @@
+// Họ tên: Đoàn Minh Thịnh - MSSV: 25810040
+fun main() {
+    val soLuong: Int = 5
+    val donGia: Double = 150000.0
+    val tienHang: Double = soLuong.toDouble() * donGia
+    val thue: Double = tienHang * 0.10
+    val tongTien: Double = tienHang + thue
+
+    println("Tổng tiền phải trả: $tongTien VNĐ")
+}
